7f05fd128b98625cf10b57e21c91d05e2dc04ea8
andreluizlq <andreluizlq1@gmail.com>
2023-03-27 12:17:15 -0300

    feat: instalação do github pages

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,92 +6,150 @@ import {
   useMediaQuery,
   Box,
 } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Page = ({ children, container = false, variant = false }) => {
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   const navigate = useNavigate();
+  const [isActive, setIsActive] = useState(false);
 
   const handleHome = () => {
     return navigate("/");
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const currentScrollPosition = window.scrollY;
+      if (currentScrollPosition) {
+        setIsActive(true);
+      } else {
+        setIsActive(false);
+      }
+    });
+  }, []);
+
   return (
     <Stack>
-      <Stack
-        alignItems="center"
-        px="1.5rem"
-        py="0.5rem"
-        flexDirection="row"
-        justifyContent="space-between"
-        sx={{
-          borderBottom: !variant
-            ? "0.5px solid #E0E0E0"
-            : "0.5px solid #2F2F32",
-          background: variant ? "#6383DD" : "#fff",
-          zIndex: "100",
-          position: "fixed",
-          left: "0",
-          top: "0",
-          width: "100%",
-        }}
-      >
-        <Typography
-          fontSize="2.625rem"
-          fontWeight="300"
-          onClick={handleHome}
-          sx={{ cursor: "pointer" }}
+      {variant && (
+        <Stack
+          alignItems="center"
+          px="1.5rem"
+          py="0.5rem"
+          flexDirection="row"
+          justifyContent="space-between"
+          sx={{
+            borderBottom: !isActive
+              ? "0.5px solid #2F2F32"
+              : "0.5px solid #E0E0E0",
+            background: !isActive ? "#6383DD" : "#fff",
+            zIndex: "100",
+            position: "fixed",
+            left: "0",
+            top: "0",
+            width: "100%",
+          }}
         >
-          alkabot
-        </Typography>
-
-        <Stack flexDirection="row" alignItems="center">
-          <Button
-            variant="contained"
-            color={!variant ? "secondary" : "primary"}
-            sx={{
-              textTransform: "none",
-              paddingX: "23px",
-              borderRadius: "2rem",
-              fontWeight: 400,
-            }}
+          <Typography
+            fontSize="2.625rem"
+            fontWeight="300"
+            onClick={handleHome}
+            sx={{ cursor: "pointer" }}
           >
-            Cadastre-se
-          </Button>
-          {!isMobile && (
-            <>
-              {!variant && (
-                <Typography
-                  ml={2}
-                  mr={4}
-                  sx={{
-                    cursor: "pointer",
-                    opacity: "60%",
-                    "&:hover": {
-                      color: "#706E72",
-                    },
-                  }}
-                >
-                  Entrar
-                </Typography>
-              )}
-              {variant && (
-                <Typography
-                  ml={2}
-                  w
-                  mr={4}
-                  sx={{
-                    cursor: "pointer",
-                    color: "#fff",
-                  }}
-                >
-                  Entrar
-                </Typography>
-              )}
-            </>
-          )}
+            alkabot
+          </Typography>
+
+          <Stack flexDirection="row" alignItems="center">
+            <Button
+              variant="contained"
+              color={!isActive ? "primary" : "secondary"}
+              sx={{
+                textTransform: "none",
+                paddingX: "23px",
+                borderRadius: "2rem",
+                fontWeight: 400,
+              }}
+            >
+              Cadastre-se
+            </Button>
+            {!isMobile && (
+              <Typography
+                ml={2}
+                mr={4}
+                sx={{
+                  cursor: "pointer",
+                  color: !isActive ? "#fff" : "primary",
+                  opacity: !isActive ? "100%" : "60%",
+                  "&:hover": {
+                    color: !isActive ? "none" : "#706E72",
+                  },
+                }}
+              >
+                Entrar
+              </Typography>
+            )}
+          </Stack>
         </Stack>
-      </Stack>
+      )}
+
+      {!variant && (
+        <Stack
+          alignItems="center"
+          px="1.5rem"
+          py="0.5rem"
+          flexDirection="row"
+          justifyContent="space-between"
+          sx={{
+            borderBottom: "0.5px solid #E0E0E0",
+            background: "#fff",
+            zIndex: "100",
+            position: "fixed",
+            left: "0",
+            top: "0",
+            width: "100%",
+          }}
+        >
+          <Typography
+            fontSize="2.625rem"
+            fontWeight="300"
+            onClick={handleHome}
+            sx={{ cursor: "pointer" }}
+          >
+            alkabot
+          </Typography>
+
+          <Stack flexDirection="row" alignItems="center">
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                textTransform: "none",
+                paddingX: "23px",
+                borderRadius: "2rem",
+                fontWeight: 400,
+              }}
+            >
+              Cadastre-se
+            </Button>
+            {!isMobile && (
+              <Typography
+                ml={2}
+                mr={4}
+                sx={{
+                  cursor: "pointer",
+                  opacity: "60%",
+                  "&:hover": {
+                    color: "#706E72",
+                  },
+                }}
+              >
+                Entrar
+              </Typography>
+            )}
+          </Stack>
+        </Stack>
+      )}
+
       <Box mt="80px">
         {container && <Container>{children}</Container>}
         {!container && children}
